6b6cacc02abc5aeb45bbf5c19102448f0a8cba6d
Javiex <javiexalonso@gmail.com>
2022-02-15 21:16:01 +0100

    Hola mundo

---
+++ Amplify/src/test/java/com/example/amplify/AmplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
