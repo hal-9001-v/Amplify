254faf5f5c96e4e8f8bf73c01df6f419dea287fd
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2022-02-19 17:00:59 +0100

    Prepare files for MySq

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,46 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
+    Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+
+
+    @GetMapping("/index")
+    public String sayHello(Model model) {
+
+        model.addAttribute("article", "So very true but will give");
+
+        logger.info("Log!");
+
+        return "index";
+    }
+
+    @GetMapping("/article")
+    public String getUsers(Model model) {
+
+
+        model.addAttribute("matar", true);
+
+        return "article";
+
     }
 
 }
