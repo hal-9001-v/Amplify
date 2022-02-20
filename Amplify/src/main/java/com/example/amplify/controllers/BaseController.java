790a7e8cd489743b692ce391454ebc9432061875
dexaxi <dexaxi12@gmail.com>
2022-02-20 21:39:21 +0100

    [FEAT] Created playlist controller

--- Amplify/src/main/java/com/example/amplify/controllers/BaseController.java
+++ Amplify/src/main/java/com/example/amplify/controllers/BaseController.java
@@ -12,12 +12,14 @@ public class BaseController {
 
     @RequestMapping( {"/", "/inicio"} )
     public String main(Model model) {
+        model.addAttribute("loggedIn", false);
         return "main_template";
+
     }
 
     @RequestMapping("/inicio_{username}")
     public String mainWithUser(Model model, @PathVariable String username){
-
+        model.addAttribute("loggedIn", true);
         model.addAttribute("username", username);
         return "main_template";
 
