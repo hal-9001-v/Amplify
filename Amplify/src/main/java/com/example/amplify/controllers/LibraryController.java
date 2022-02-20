95ba0b21a8c510abe0a16a12fd111dbd23415869
dexaxi <dexaxi12@gmail.com>
2022-02-20 21:10:50 +0100

    [FEAT] added library controller skeleton

--- Amplify/src/main/java/com/example/amplify/controllers/LibraryController.java
+++ Amplify/src/main/java/com/example/amplify/controllers/LibraryController.java
@@ -2,9 +2,44 @@ package com.example.amplify.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LibraryController {
 
+    @RequestMapping("/biblioteca")
+    public String defaultLibrary(Model model){
+        return "library_template";
+    }
+    @RequestMapping("/biblioteca_{username}")
+    public String viewUserLibrary(Model model, @PathVariable String username){
+        model.addAttribute("username", username);
+        return "library_template";
+    }
 
+    @RequestMapping("biblioteca/{username}/playlists")
+    public String viewUserPlaylists(Model model, @PathVariable String username){
+        model.addAttribute("username", username);
+        return "playlist_template";
+    }
+    
+    @RequestMapping("biblioteca/{username}/songs")
+    public String viewUserSongs(Model model, @PathVariable String username){
+        model.addAttribute("username", username);
+        return "song_template";
+    }
+
+    @RequestMapping("biblioteca/{username}/albums")
+    public String viewUserAlbums(Model model, @PathVariable String username){
+        model.addAttribute("username", username);
+        return "album_template";
+    }
+
+    @RequestMapping("biblioteca/{username}/artists")
+    public String viewUserArtists(Model model, @PathVariable String username){
+        model.addAttribute("username", username);
+        return "artis_template";
+    }
 }
