1ec3c61efd31815aaa5a6eed618789d8ed292ae4
dexaxi <dexaxi12@gmail.com>
2022-03-25 20:31:48 +0100

    [FEAT] Relocated amplify app project and added amplify internal service project

---
+++ AmplifyInternalServiceProject/src/test/java/com/example/amplifyinternalservice/AmplifyInternalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amplifyinternalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplifyInternalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
