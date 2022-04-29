7c28ebe6b0c9b4de5beea38c2dbd4b7605847a41
dexaxi <dexaxi12@gmail.com>
2022-04-29 22:34:27 +0200

    [FEAT] Added cache + fixed bug with db loader

--- AmplifyApplicationProject/Amplify/src/main/java/com/example/amplify/AmplifyApplication.java
+++ AmplifyApplicationProject/Amplify/src/main/java/com/example/amplify/AmplifyApplication.java
@@ -3,13 +3,51 @@ package com.example.amplify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 @SpringBootApplication
+@EnableCaching
+@EnableHazelcastHttpSession
 public class AmplifyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmplifyApplication.class, args);
     }
 
+    @Bean
+    public CacheManager cacheManager(HazelcastInstance hazelcastInstance)
+    {
+        return new HazelcastCacheManager(hazelcastInstance);
+    }
 
+    @Bean
+    public HazelcastInstance hazelcastInstance (Config config){
+        return Hazelcast.newHazelcastInstance(config);
+    }
+
+    @Bean
+    public Config config()
+    {
+        Config config = new Config();
+
+        JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+        joinConfig.getMulticastConfig().setEnabled(true);
+
+        MapConfig usersMapConfig = new MapConfig().setName("amplify");
+
+
+        config.addMapConfig(usersMapConfig);
+
+        return config;
+    }
 }
--- AmplifyApplicationProject/Amplify/src/main/java/com/example/amplify/services/DatabaseLoader.java
+++ AmplifyApplicationProject/Amplify/src/main/java/com/example/amplify/services/DatabaseLoader.java
@@ -61,153 +61,154 @@ public class DatabaseLoader {
         albumRepository.deleteAll();
         playlistRepository.deleteAll();
          */
-        if(loadDB) {
+        if (loadDB) {
+            if (artistServices.findByName("Porta").isEmpty()) {
+                System.out.println("HACIENDO MOVIDAS DE LA BD ");
+                User user = new User("dexaxi", passwordEncoder.encode("123"), "dexaxi12@gmail.com", "USER", "ADMIN");
+                Artist artist0 = new Artist("Porta");
+                Artist artist1 = new Artist("Kiko Rivera");
+                Artist artist2 = new Artist("Imagine Dragons");
+                Album album0 = new Album("He cambiado");
+                Album album1 = new Album("No es Cuesion de Edades");
+                Album album2 = new Album("Los Mejores Exitos de Kiko Rivera");
+                Album album3 = new Album("Origins");
 
-            System.out.println("HACIENDO MOVIDAS DE LA BD ");
-            User user = new User("dexaxi", passwordEncoder.encode("123"), "dexaxi12@gmail.com", "USER", "ADMIN");
-            Artist artist0 = new Artist("Porta");
-            Artist artist1 = new Artist("Kiko Rivera");
-            Artist artist2 = new Artist("Imagine Dragons");
-            Album album0 = new Album("He cambiado");
-            Album album1 = new Album("No es Cuesion de Edades");
-            Album album2 = new Album("Los Mejores Exitos de Kiko Rivera");
-            Album album3 = new Album("Origins");
+                artistRepository.save(artist0);
+                artistRepository.save(artist1);
+                artistRepository.save(artist2);
+                albumRepository.save(album0);
+                albumRepository.save(album1);
+                albumRepository.save(album2);
+                albumRepository.save(album3);
 
-            artistRepository.save(artist0);
-            artistRepository.save(artist1);
-            artistRepository.save(artist2);
-            albumRepository.save(album0);
-            albumRepository.save(album1);
-            albumRepository.save(album2);
-            albumRepository.save(album3);
-
-            String[] genre = new String[6];
-            genre[0] = "CHILL";
-            genre[1] = "GAMING";
-            genre[2] = "SPORT";
-            genre[3] = "INTIMATE";
-            genre[4] = "DRIVING";
-            genre[5] = "PODCAST";
+                String[] genre = new String[6];
+                genre[0] = "CHILL";
+                genre[1] = "GAMING";
+                genre[2] = "SPORT";
+                genre[3] = "INTIMATE";
+                genre[4] = "DRIVING";
+                genre[5] = "PODCAST";
 
 
-            ArrayList<SongFile> sfs = new ArrayList<SongFile>();
-            int songTotal = 10;
-            for (int i = 0; i < songTotal; i++) {
-                InputStream input1 = getClass().getClassLoader().getResourceAsStream("songs/Cancion1.ogg");
-                sfs.add(new SongFile(BlobProxy.generateProxy(input1, input1.available()), null));
-            }
-
-            for (int i = 0; i < songTotal; i++) {
-                Song s = new Song(GenerateSongName(), genre[(int) (Math.random() * 6)], null);
-                switch ((int) (Math.random() * 4)) {
-                    case 0:
-                        s.setAlbum(album0);
-                        s.setArtist(artist0);
-                        break;
-                    case 1:
-                        s.setAlbum(album1);
-                        s.setArtist(artist0);
-                        break;
-                    case 2:
-                        s.setAlbum(album2);
-                        s.setArtist(artist1);
-                        break;
-                    case 3:
-                        s.setAlbum(album3);
-                        s.setArtist(artist2);
-
-                        break;
-
+                ArrayList<SongFile> sfs = new ArrayList<SongFile>();
+                int songTotal = 10;
+                for (int i = 0; i < songTotal; i++) {
+                    InputStream input1 = getClass().getClassLoader().getResourceAsStream("songs/Cancion1.ogg");
+                    sfs.add(new SongFile(BlobProxy.generateProxy(input1, input1.available()), null));
                 }
-                songRepo.save(s);
-            }
 
-            List<Song> allSongs = songServices.findAll();
+                for (int i = 0; i < songTotal; i++) {
+                    Song s = new Song(GenerateSongName(), genre[(int) (Math.random() * 6)], null);
+                    switch ((int) (Math.random() * 4)) {
+                        case 0:
+                            s.setAlbum(album0);
+                            s.setArtist(artist0);
+                            break;
+                        case 1:
+                            s.setAlbum(album1);
+                            s.setArtist(artist0);
+                            break;
+                        case 2:
+                            s.setAlbum(album2);
+                            s.setArtist(artist1);
+                            break;
+                        case 3:
+                            s.setAlbum(album3);
+                            s.setArtist(artist2);
 
+                            break;
 
-            user.setSongs(allSongs);
-            userRepo.save(user);
+                    }
+                    songRepo.save(s);
+                }
 
-            artist0 = artistServices.findByName("Porta").get(0);
-            artist1 = artistServices.findByName("Kiko Rivera").get(0);
-            artist2 = artistServices.findByName("Imagine Dragons").get(0);
-            album0 = albumServices.findByName("He cambiado").get(0);
-            album1 = albumServices.findByName("No es Cuesion de Edades").get(0);
-            album2 = albumServices.findByName("Los Mejores Exitos de Kiko Rivera").get(0);
-            album3 = albumServices.findByName("Origins").get(0);
-
-            Playlist playlist = new Playlist("Lofi Chillstep");
-            playlist.setUser(user);
-            Playlist playlist1 = new Playlist("Sadness and Sorrow MIX");
-            playlist1.setUser(user);
-            Playlist playlist2 = new Playlist("Mi musica");
-            playlist2.setUser(user);
-
-            ArrayList<Playlist> playlistList = new ArrayList<Playlist>();
-            playlistList.add(playlist);
-            playlistList.add(playlist1);
-            playlistList.add(playlist2);
-            user.setPlaylists(playlistList);
-
-            ArrayList<Song> playList0songList = new ArrayList<>();
-            ArrayList<Song> playlist1songList = new ArrayList<>();
-            ArrayList<Song> playlist2songList = new ArrayList<>();
-            ArrayList<Song> album0songList = new ArrayList<>();
-            ArrayList<Song> album1songList = new ArrayList<>();
-            ArrayList<Song> album2songList = new ArrayList<>();
-            ArrayList<Song> album3songList = new ArrayList<>();
-
-            for (int i = 0; i < songTotal / 4; i++) {
-                playList0songList.add(songServices.findAll().get(i));
-                playlist1songList.add(songServices.findAll().get(i + songTotal / 4));
-                playlist2songList.add(songServices.findAll().get(i + songTotal / 4 + songTotal / 4));
-            }
-
-            for (int i = 0; i < songTotal / 8; i++) {
-                album0songList.add(songServices.findAll().get(i));
-                album1songList.add(songServices.findAll().get(i + songTotal / 4));
-                album2songList.add(songServices.findAll().get(i + songTotal / 4 + songTotal / 4));
-                album3songList.add(songServices.findAll().get(i + songTotal / 4 + songTotal / 4 + songTotal / 4));
-            }
+                List<Song> allSongs = songServices.findAll();
 
 
-            playlist.setSongs(playList0songList);
-            playlist1.setSongs(playlist1songList);
-            playlist2.setSongs(playlist2songList);
-            album0.setSongs(album0songList);
-            album0.setArtist(artist0);
-            album1.setSongs(album1songList);
-            album1.setArtist(artist0);
-            album2.setSongs(album2songList);
-            album2.setArtist(artist1);
-            album3.setSongs(album3songList);
-            album3.setArtist(artist2);
+                user.setSongs(allSongs);
+                userRepo.save(user);
 
-            albumRepository.save(album0);
-            albumRepository.save(album1);
-            albumRepository.save(album2);
-            albumRepository.save(album3);
+                artist0 = artistServices.findByName("Porta").get(0);
+                artist1 = artistServices.findByName("Kiko Rivera").get(0);
+                artist2 = artistServices.findByName("Imagine Dragons").get(0);
+                album0 = albumServices.findByName("He cambiado").get(0);
+                album1 = albumServices.findByName("No es Cuesion de Edades").get(0);
+                album2 = albumServices.findByName("Los Mejores Exitos de Kiko Rivera").get(0);
+                album3 = albumServices.findByName("Origins").get(0);
 
-            ArrayList<Album> albumList = new ArrayList<Album>();
-            ArrayList<Artist> artistList = new ArrayList<Artist>();
-            albumList.add(album0);
-            albumList.add(album1);
-            albumList.add(album3);
-            artistList.add(artist0);
-            artistList.add(artist1);
-            artistList.add(artist2);
+                Playlist playlist = new Playlist("Lofi Chillstep");
+                playlist.setUser(user);
+                Playlist playlist1 = new Playlist("Sadness and Sorrow MIX");
+                playlist1.setUser(user);
+                Playlist playlist2 = new Playlist("Mi musica");
+                playlist2.setUser(user);
 
-            user.setAlbums(albumList);
-            user.setArtists(artistList);
-            playlistRepository.save(playlist);
-            playlistRepository.save(playlist1);
-            playlistRepository.save(playlist2);
-            userRepo.save(user);
+                ArrayList<Playlist> playlistList = new ArrayList<Playlist>();
+                playlistList.add(playlist);
+                playlistList.add(playlist1);
+                playlistList.add(playlist2);
+                user.setPlaylists(playlistList);
+
+                ArrayList<Song> playList0songList = new ArrayList<>();
+                ArrayList<Song> playlist1songList = new ArrayList<>();
+                ArrayList<Song> playlist2songList = new ArrayList<>();
+                ArrayList<Song> album0songList = new ArrayList<>();
+                ArrayList<Song> album1songList = new ArrayList<>();
+                ArrayList<Song> album2songList = new ArrayList<>();
+                ArrayList<Song> album3songList = new ArrayList<>();
+
+                for (int i = 0; i < songTotal / 4; i++) {
+                    playList0songList.add(songServices.findAll().get(i));
+                    playlist1songList.add(songServices.findAll().get(i + songTotal / 4));
+                    playlist2songList.add(songServices.findAll().get(i + songTotal / 4 + songTotal / 4));
+                }
+
+                for (int i = 0; i < songTotal / 8; i++) {
+                    album0songList.add(songServices.findAll().get(i));
+                    album1songList.add(songServices.findAll().get(i + songTotal / 4));
+                    album2songList.add(songServices.findAll().get(i + songTotal / 4 + songTotal / 4));
+                    album3songList.add(songServices.findAll().get(i + songTotal / 4 + songTotal / 4 + songTotal / 4));
+                }
 
 
-            for (int i = 0; i < songTotal; i++) {
-                sfs.get(i).setSong(allSongs.get(i));
-                songFileRepo.save(sfs.get(i));
+                playlist.setSongs(playList0songList);
+                playlist1.setSongs(playlist1songList);
+                playlist2.setSongs(playlist2songList);
+                album0.setSongs(album0songList);
+                album0.setArtist(artist0);
+                album1.setSongs(album1songList);
+                album1.setArtist(artist0);
+                album2.setSongs(album2songList);
+                album2.setArtist(artist1);
+                album3.setSongs(album3songList);
+                album3.setArtist(artist2);
+
+                albumRepository.save(album0);
+                albumRepository.save(album1);
+                albumRepository.save(album2);
+                albumRepository.save(album3);
+
+                ArrayList<Album> albumList = new ArrayList<Album>();
+                ArrayList<Artist> artistList = new ArrayList<Artist>();
+                albumList.add(album0);
+                albumList.add(album1);
+                albumList.add(album3);
+                artistList.add(artist0);
+                artistList.add(artist1);
+                artistList.add(artist2);
+
+                user.setAlbums(albumList);
+                user.setArtists(artistList);
+                playlistRepository.save(playlist);
+                playlistRepository.save(playlist1);
+                playlistRepository.save(playlist2);
+                userRepo.save(user);
+
+
+                for (int i = 0; i < songTotal; i++) {
+                    sfs.get(i).setSong(allSongs.get(i));
+                    songFileRepo.save(sfs.get(i));
+                }
             }
         }
     }
